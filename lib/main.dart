51f67804572b0e10a52ef488d4a9e7ccfed8c23d
BennyC1 <160693849+BennyC1@users.noreply.github.com>
2025-03-07 11:13:28 +0700

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:pilem/screens/favorite_screen.dart';
 import 'package:pilem/screens/home_screen.dart';
+import 'package:pilem/screens/search_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,6 +17,47 @@ class MainApp extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Pilem',
         theme: ThemeData(primaryColorDark: Colors.blue),
-        home: const HomeScreen());
+        home: const MainScreen());
   }
+}
+
+class MainScreen extends StatelessWidget {
+  const MainScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final controller = Get.put(NavigationController());
+
+    return Scaffold(
+    bottomNavigationBar: Obx(
+      () => NavigationBar(
+          height: 80,
+          elevation: 0,
+          selectedIndex: controller.selectedIndex.value,
+          onDestinationSelected: (index) =>
+              controller.selectedIndex.value = index,
+          backgroundColor: Colors.white,
+          indicatorColor: Colors.deepOrangeAccent,
+          destinations: const [
+            NavigationDestination(icon: Icon(Icons.home), label: 'Home'),
+            NavigationDestination(icon: Icon(Icons.heart_broken), label: 'Favorite'),
+            NavigationDestination(
+                icon: Icon(Icons.search), label: 'Search'),
+
+                
+          ]),
+      ),
+      body: Obx(() => controller.screens[controller.selectedIndex.value]),
+    );
+  }
+}
+
+class NavigationController extends GetxController {
+  final Rx<int> selectedIndex = 0.obs;
+
+  final screens = [
+    const HomeScreen(),
+    const FavoriteScreen(),
+    const SearchScreen()
+  ];
 }
